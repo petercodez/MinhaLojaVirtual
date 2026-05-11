e785db50e01d6255d969497b667f6f8c1c837720
literallynoonefr <pedrozcsl@gmail.com>
2026-05-11 19:49:04 -0300

    JWT/Packages

--- MinhaLoja.Core/Data/AppDbContext.cs
+++ MinhaLoja.Core/Data/AppDbContext.cs
@@ -2,13 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using MinhaLoja.Core.Models;
 
 namespace MinhaLoja.Core.Data;
 
 // O segredo está aqui: herdar de DbContext
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<IdentityUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
@@ -17,7 +19,6 @@ public class AppDbContext : DbContext
     // Se essa linha não estiver aqui, o EF acha que o banco está vazio
     public DbSet<Produto> Produtos { get; set; }
     public DbSet<Categoria> Categorias { get; set; }
-    public DbSet<Usuario> Usuarios { get; set; }
     public DbSet<Cliente> Clientes { get; set; }
     public DbSet<Pedido> Pedidos { get; set; }
     public DbSet<PedidoItem> PedidoItens { get; set; }
--- MinhaLoja.web/Program.cs
+++ MinhaLoja.web/Program.cs
@@ -1,8 +1,38 @@
 using MinhaLoja.web.Components;
 using MinhaLoja.Core.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurações do JWT
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var key = Encoding.ASCII.GetBytes(jwtSettings["Secret"]!);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidateAudience = true,
+        ValidAudience = jwtSettings["Audience"],
+        ValidateLifetime = true
+    };
+});
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -26,6 +56,8 @@ builder.Services.AddScoped(sp => new HttpClient {
     BaseAddress = new Uri("https://localhost:5269/") // Ajuste para a sua porta real
 });
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,7 +73,10 @@ app.UseSwaggerUI();
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
-app.UseAntiforgery();
+app.UseAuthentication(); // 1º: Identifica QUEM é o utilizador (Lê o Token)
+app.UseAuthorization();  // 2º: Verifica O QUE o utilizador pode fazer (Permissões)
+
+app.UseAntiforgery(); // 3º: Protege os formulários Blazor (precisa de saber quem é o utilizador)
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
