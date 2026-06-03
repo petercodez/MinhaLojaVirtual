b844b9bcc54f2eabd1e45518be65eb6a8e053c38
literallynoonefr <pedrozcsl@gmail.com>
2026-06-02 22:16:37 -0300

    Melhorias no Carrinho+Roteamento

--- MinhaLoja.web/Services/CarrinhoService.cs
+++ MinhaLoja.web/Services/CarrinhoService.cs
@@ -33,6 +33,34 @@ public class CarrinhoService
         }
     }
 
+    public async Task RemoverDoCarrinho(ProdutoReadDTO produto)
+    {
+        // 1. Pega a lista atual do cofre
+        var carrinho = await ObterCarrinho();
+        
+        // 2. Procura a guitarra exata que o usuário quer apagar
+        var itemARemover = carrinho.FirstOrDefault(p => p.Id == produto.Id);
+        
+        if (itemARemover != null)
+        {
+            // 3. Remove da lista e salva a lista atualizada de volta no cofre
+            carrinho.Remove(itemARemover);
+            await _localStorage.SetItemAsync(CartKey, carrinho);
+            
+            // 4. Dispara o alarme para o NavMenu atualizar o numerozinho amarelo lá em cima!
+            OnChange?.Invoke(); 
+        }
+    }
+    
+    public async Task LimparCarrinho()
+    {
+        // Remove a chave inteira do cofre do navegador
+        await _localStorage.RemoveItemAsync(CartKey);
+        
+        // Toca o alarme para o Menu Superior zerar o número
+        OnChange?.Invoke();
+    }
+
     public async Task<List<ProdutoReadDTO>> ObterCarrinho()
     {
         // Tenta ler do cofre. Se voltar nulo, cria uma lista vazia.
